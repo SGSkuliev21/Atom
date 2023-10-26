9d154de48e1085e4853fc9ad7ee6f718492fd0cc
VDVichev21 <VDVichev21@codingburgas.bg>
2023-10-26 23:00:42 +0300

    Add raylib

---
+++ Atom/Atom/Atom.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <raylib.h>
+
+
+const int screenWidth = 1000;
+const int screenHeight = 750;
+
+int main()
+{
+    InitWindow(screenWidth, screenHeight, "Atom");
+    while (!WindowShouldClose())  
+    {
+        BeginDrawing(); //start drawing
+        DrawText("ATOM", 450, 350, 50, WHITE);
+
+        EndDrawing();    
+    }
+    CloseWindow();        // Close window and OpenGL context
+
+}
