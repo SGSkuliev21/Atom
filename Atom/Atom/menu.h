56928887a7cce0da750eefa4c8f42182bd6112b8
DKByandov21 <dkbyandov21@codingburgas.bg>
2023-11-12 16:10:06 +0200

    Fix bugs

--- Atom/Atom/menu.h
+++ Atom/Atom/menu.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <iostream>
+#include "raylib.h"
+
+enum SceneType
+{
+    MAIN_MENU,
+    INFO_MENU,
+};
+
+void mainMenu(SceneType& sceneState);
+void secondMenu(SceneType& sceneState);
